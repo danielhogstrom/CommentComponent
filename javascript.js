d8d7b5ccc72d28811b98834b89568ff5cb9df47f
danielhogstrom <daniel.hogstrom@gmail.com>
2022-03-10 12:05:52 +0100

    add validation to name and comment field

--- javascript.js
+++ javascript.js
@@ -1,14 +1,45 @@
 const inputButton = document.getElementById("inputButton");
 const inputArea = document.getElementById("inputArea");
 const commentsList = document.getElementById("commentsList");
+const info = document.getElementById("info");
+const infoText = document.getElementById("infoText");
+const nameArea = document.getElementById("nameArea");
 let comments = [];
 
 inputButton.addEventListener("click", (event) => {
-  postComment(inputArea.value);
-  console.log(comments);
+  postComment(inputArea.value, nameArea.value);
+  inputArea.value = "";
   event.preventDefault();
 });
 
-const postComment = (comment) => {
+const postComment = (comment, name) => {
+  if (name === "" && comment === "") {
+    info.classList.remove("hide");
+    infoText.innerHTML = "Both fields are empty";
+    return;
+  }
+  if (name === "Oscar") {
+    info.classList.remove("hide");
+    infoText.innerHTML = "Oscar is not a valid name. </br>Menade du: Peter ?";
+    return;
+  }
+  if (comment === "") {
+    info.classList.remove("hide");
+    infoText.innerHTML = "Please enter a comment";
+    return;
+  }
+  if (name === "") {
+    info.classList.remove("hide");
+    infoText.innerHTML = "Please enter a name";
+    return;
+  }
+
+  info.classList.add("hide");
+  infoText.innerHTML = "";
   comments.unshift(comment);
+  const li = document.createElement("li");
+  li.innerHTML = `${name} said: </br> ${comment}`;
+  commentsList.appendChild(li);
+  console.log(li);
+  console.log(comment);
 };
